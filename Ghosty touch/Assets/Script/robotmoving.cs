dd43cb369096067f235f1e109439714cc27f76b7
duprezf <florian.duprez@student.uclouvain.be>
2018-09-06 21:32:11 +0200

    Add project

---
+++ Ghosty touch/Assets/Script/robotmoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotmoving : MonoBehaviour {
+
+    public float speed;
+    Rigidbody2D mybody;
+
+    void Start()
+    {
+        mybody = this.GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 myVel = mybody.velocity;
+        myVel.y = speed;
+        mybody.velocity = myVel;
+    }
+}
+
+
